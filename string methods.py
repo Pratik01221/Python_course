1fe23c38a747c5904bc6a628442bff1d3f2d8d40
Pratik01221 <144597734+Pratik01221@users.noreply.github.com>
2023-09-19 06:47:39 -0400

    Add files via upload

---
+++ string methods.py
@@ -0,0 +1,19 @@
+# UPPER() :
+str1="pratik narayan garad"
+print(str1.upper())
+# LOWER():
+str1="PRATIK NARAYAN GARAD"
+print(str1.lower())
+#RSTRIP():
+str1="Hellow::::::::"
+print(str1.rstrip(":"))
+#REPLACE():
+str1="YCIS SATARA"
+print(str1.replace("YCIS","FORENSIC"))
+# SPLIT():
+a = "!!!Harry!! !!!!!!!!! Harry"
+print(a.split(" "))
+#CAPITALIZE():
+str1="pratik narayan garad"
+print(str1.capitalize())
+
